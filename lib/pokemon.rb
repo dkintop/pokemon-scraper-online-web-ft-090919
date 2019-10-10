1ddd0d9a818c9d9ce0d88252063168112ae2ada2
Doug Kintop <dkintop@hotmail.com>
2019-10-10 21:47:32 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -9,6 +9,16 @@ class Pokemon
     @db = db 
   end 
   
-  
+   def self.save 
+    if self.id
+      self.update
+    else
+      sql = <<-SQL 
+      INSERT INTO students (name, grade)
+      VALUES (?, ?)
+      SQL
+      DB[:conn].execute(sql, self.name, self.grade)
+       @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+    end
   
 end
