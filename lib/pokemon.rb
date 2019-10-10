30d0a2e91b0d6e133ade7171b3bf7bcc4273338b
Doug Kintop <dkintop@hotmail.com>
2019-10-10 21:46:53 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,12 @@
+require 'pry'
 class Pokemon
+  attr_accessor :id, :name, :type, :db 
+  def initialize(pokemon)
+    binding.pry
+    @id = id 
+    @name = name 
+    @type = type 
+    @db = db 
+  end 
+  
 end
